b9fa748b0843239d889db3c3354bc14e226240a5
1241790664@qq.com <1241790664@qq.com>
2019-08-01 23:27:12 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = new express();
 app.use(express.static('./page/'));
-app.listen(12306,function () {
+app.listen(12306, function() {
     console.log('服务已启动')
 })
